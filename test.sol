0c8dba7331d0cc84ff4de71d6d0a88180e9ee904
anonyDev <phuochh@nortstudio.vn>
2024-03-18 08:45:14 +0700

    update swap native token  flow

--- test.sol
+++ test.sol
@@ -8,9 +8,6 @@ contract MultiSwap {
     // Mapping lưu trữ tỷ giá hối đoái cho mỗi cặp mã thông báo
     mapping(address => mapping(address => uint256)) public rates;
 
-    // Mapping lưu trữ địa chỉ token cho mỗi native token (BNB, ETH)
-    mapping(address => address) public nativeTokens;
-
     // Địa chỉ admin
     address public admin;
 
@@ -21,55 +18,60 @@ contract MultiSwap {
     }
 
     // Hàm cho phép admin thiết lập tỷ giá hối đoái cho một cặp mã thông báo
-    function setRate(address tokenA, address tokenB, uint256 rate) external onlyAdmin {
-        require(tokenA != address(0) && tokenB != address(0), "Invalid token addresses");
-        rates[tokenA][tokenB] = rate;
+    function setRate(address _tokenA, address _tokenB, uint256 _rate) external onlyAdmin {
+        require(_tokenA != address(0) && _tokenB != address(0), "Invalid token addresses");
+        rates[_tokenA][_tokenB] = _rate;
     }
 
-    // Hàm cho phép admin thiết lập địa chỉ token cho native token (BNB, ETH)
-    function setNativeToken(address token, address nativeToken) external onlyAdmin {
-        require(token != address(0) && nativeToken != address(0), "Invalid token addresses");
-        nativeTokens[token] = nativeToken;
+    function swap(address _tokenIn, address _tokenOut, uint256 _amountIn) external payable {
+        require(_tokenIn != address(0) && _tokenOut != address(0), "Invalid token addresses");
+        require(_amountIn > 0, "Invalid amount");
+
+        uint256 amountOut = calculateAmountOut(_tokenIn, _tokenOut, _amountIn);
+        
+        _handleAmountIn(_tokenIn, _amountIn);
+        _handleAmountOut(_tokenOut, amountOut);
+
+        emit Swap(msg.sender, _tokenIn, _tokenOut, _amountIn, amountOut);
     }
 
-    // Hàm cho phép người dùng swap token
-    function swap(address tokenIn, address tokenOut, uint256 amountIn) external {
-        require(tokenIn != address(0) && tokenOut != address(0), "Invalid token addresses");
-        require(amountIn > 0, "Invalid amount");
-
-        // Tính toán số lượng token out nhận được
-        uint256 amountOut = calculateAmountOut(tokenIn, tokenOut, amountIn);
-
-        // Chuyển token in cho hợp đồng
-        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
-
-        // Chuyển token out cho người dùng
-        IERC20(tokenOut).transfer(msg.sender, amountOut);
-
-        emit Swap(msg.sender, tokenIn, tokenOut, amountIn, amountOut);
-    }
-
-    // Hàm tính toán số lượng token out nhận được
-    function calculateAmountOut(address tokenIn, address tokenOut, uint256 amountIn) public view returns (uint256) {
-        uint256 rateIn = getRate(tokenIn);
-        uint256 rateOut = getRate(tokenOut);
+    function calculateAmountOut(address _tokenIn, address _tokenOut, uint256 _amountIn) public view returns (uint256) {
+        uint256 rateIn = getRate(_tokenIn,_tokenOut);
+        uint256 rateOut = getRate(_tokenOut,_tokenIn);
 
         require(rateIn > 0 && rateOut > 0, "Invalid rates");
 
-        return amountIn * rateOut / rateIn;
+        return _amountIn * rateOut / rateIn;
     }
 
-    // Hàm lấy tỷ giá hối đoái cho một mã thông báo
-    function getRate(address token) public view returns (uint256) {
-        address nativeToken = nativeTokens[token];
-        require(nativeToken != address(0), "Invalid native token");
-
-        return rates[token][nativeToken];
+    function _handleAmountIn(address _tokenIn, uint _amountIn) internal {
+        if(_isNativeToken(_tokenIn)) {
+            require(_amountIn == msg.value, "Amount must be equal to msg.value");        
+            return;
+        }
+        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
     }
 
-    // Modifier chỉ cho phép admin thực thi hàm
+    function _handleAmountOut(address _tokenOut, uint _amountOut) internal {
+        if(_isNativeToken(_tokenOut)) {
+            (bool sent,) = msg.sender.call{value: _amountOut}("");
+            require(sent, "Failed to send Ether");  
+            return;
+        }
+        IERC20(_tokenOut).transfer(msg.sender, _amountOut);
+    }
+
+    function getRate(address _token1,address _token2) public view returns (uint256) {
+        return rates[_token1][_token2];
+    }
+
+    // Modifier only admin execute function
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only admin can call this function");
         _;
+    }
+
+    function _isNativeToken(address _address) internal pure returns(bool) {
+        return _address == address(0);
     }
 }
